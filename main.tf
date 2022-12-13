095fca73d9cbcd803b9490ffe53f7fff02cae24a
Renan L <renanlopesdasilva@gmail.com>
2022-12-12 23:46:00 -0300

    update init terraform

---
+++ main.tf
@@ -0,0 +1,4 @@
+resources "google_storage_bucket" "gcs_bucket"{
+    name = "testebucket1111"
+    location = var.region
+}
---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+project_id = "datapipeline01"
+region     = "us-central1"
